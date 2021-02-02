aeabf416119f7cda4c0056a3a89cf810afbdab21
Wynand Pieterse <wpieterse2825@gmail.com>
2021-02-02 14:15:15 +0200

    Refactored the Doom 3 Map parser a bit.

--- doom_3_map/main.cpp
+++ doom_3_map/main.cpp
@@ -1,5 +1,3 @@
-#include <immintrin.h>
-
 #include <fstream>
 #include <iostream>
 
@@ -17,15 +15,24 @@ int main(int argc, char** argv) {
 
     if (runfiles == nullptr) {
         std::cout << "ERROR: Failed to initialize run-files : " << runfiles_error << std::endl;
+        return 1;
     }
 
     std::string  map_path = runfiles->Rlocation("scrapbook/doom_3_map/delta4.pb");
     std::fstream map_file {map_path, std::ios::in | std::ios::binary};
-    Map          map_object;
+    if (!map_file) {
+        std::cout << "ERROR: Failed to open map-file : " << map_path << std::endl;
+        return 1;
+    }
 
-    map_object.ParseFromIstream(&map_file);
+    Map map_object;
+    if (!map_object.ParseFromIstream(&map_file)) {
+        std::cout << "ERROR: Failed to parse map file" << std::endl;
+        return 1;
+    }
 
     uint64_t entities = 0;
+    uint64_t primitives = 0;
     uint64_t brushes  = 0;
     uint64_t sides    = 0;
     uint64_t patches  = 0;
@@ -35,12 +42,12 @@ int main(int argc, char** argv) {
         for (const auto& primitive : entity.primitives()) {
             auto primitive_type = primitive.primitive_type_case();
 
+            primitives++;
             if (primitive_type == Primitive::kBrush) {
                 auto brush = primitive.brush();
 
                 brushes++;
                 for (const auto& side : brush.sides()) {
-                    auto comparend_plane = side.plane();
                     sides++;
                 }
             } else if (primitive_type == Primitive::kPatch) {
@@ -49,10 +56,11 @@ int main(int argc, char** argv) {
         }
     }
 
-    std::cout << "entities : " << entities << std::endl;
-    std::cout << "brushes  : " << brushes << std::endl;
-    std::cout << "sides    : " << sides << std::endl;
-    std::cout << "patches  : " << patches << std::endl;
+    std::cout << "entities   : " << entities << std::endl;
+    std::cout << "primitives : " << primitives << std::endl;
+    std::cout << "brushes    : " << brushes << std::endl;
+    std::cout << "sides      : " << sides << std::endl;
+    std::cout << "patches    : " << patches << std::endl;
 
     google::protobuf::ShutdownProtobufLibrary();
     return 0;
