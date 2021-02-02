ba3a3ed6a625380b585f1ae613ac0e43eec5cd7d
Wynand Pieterse <wpieterse2825@gmail.com>
2021-02-02 14:16:54 +0200

    Refactored the modern terminal example.

--- modern_terminal/main.cpp
+++ modern_terminal/main.cpp
@@ -15,23 +15,23 @@
 
 using namespace rxterm;
 
-auto renderToTerm = [](auto const& vt, unsigned const w, Component const& c) {
-    // TODO: get actual terminal width
-    return vt.flip(c.render(w).toString());
+auto RenderToTerminal = [](auto const& terminal, unsigned const width, Component const& component) {
+    // TODO(wpieterse): get actual terminal width
+    return terminal.flip(component.render(width).toString());
 };
 
 int main() {
     using namespace std::chrono_literals;
     using namespace std::string_literals;
 
-    VirtualTerminal vt;
+    VirtualTerminal terminal;
 
-    auto superProgressBar = [](auto x, auto y, auto z) -> StackLayout<> {
+    auto super_progress_bar = [](auto x, auto y, auto z) -> StackLayout<> {
         if (x > 0.75) {
             return {StackLayout<> {
               FlowLayout<> {
-                Text(Style{FontColor::Green}, "INFO: "),
-                Text(Style{FontColor::White}, "Finalizing                                                         "),
+                Text(Style {FontColor::Green}, "INFO: "),
+                Text(Style {FontColor::White}, "Finalizing                                                         "),
               },
             }};
         }
@@ -39,30 +39,31 @@ int main() {
         if (x > 0.5) {
             return {StackLayout<> {
               FlowLayout<> {
-                Text(Style{FontColor::Green}, "INFO: "),
-                Text(Style{FontColor::White}, "Building 1                                                         "),
+                Text(Style {FontColor::Green}, "INFO: "),
+                Text(Style {FontColor::White}, "Building 1                                                         "),
               },
               FlowLayout<> {
-                Text(Style{FontColor::Green}, "INFO: "),
-                Text(Style{FontColor::White}, "Building 2                                                         "),
+                Text(Style {FontColor::Green}, "INFO: "),
+                Text(Style {FontColor::White}, "Building 2                                                         "),
               },
               FlowLayout<> {
-                Text(Style{FontColor::Green}, "INFO: "),
-                Text(Style{FontColor::White}, "Building 3                                                         "),
+                Text(Style {FontColor::Green}, "INFO: "),
+                Text(Style {FontColor::White}, "Building 3                                                         "),
               },
             }};
         }
 
         return {StackLayout<> {
           FlowLayout<> {
-                Text(Style{FontColor::Green}, "INFO: "),
-                Text(Style{FontColor::White}, "Analyzing build graph...                                                         "),
-              },
+            Text(Style {FontColor::Green}, "INFO: "),
+            Text(Style {FontColor::White}, "Analyzing build graph...                                                         "),
+          },
         }};
     };
 
-    for (int i = 0; i < 101; ++i) {
-        vt = renderToTerm(vt, 120, superProgressBar(0.01 * i, 0.02 * i, 0.03 * i));
+    for (auto index = 0; index < 101; ++index) {
+        terminal = RenderToTerminal(terminal, 120, super_progress_bar(0.01 * index, 0.02 * index, 0.03 * index));
+
         std::this_thread::sleep_for(75ms);
     }
 
