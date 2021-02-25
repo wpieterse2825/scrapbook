eb4ba0d4bb80d2110b0124f56b94bde90b5c8712
Wynand Pieterse <wpieterse2825@gmail.com>
2021-02-25 10:49:46 +0200

    Removed all useless code.

--- code/common/sys_main.c
+++ code/common/sys_main.c
@@ -8,9 +8,6 @@
 #include "server/sv_public.h"
 #include "client/cl_public.h"
 
-#include "common/allocator.h"
-#include "common/jobs.h"
-
 void System_Error(const char* message, ...) {
     char    formatted_message[16 * 1024] = {0};
     va_list argument_list                = {};
@@ -25,28 +22,7 @@ void System_Error(const char* message, ...) {
     exit(1);
 }
 
-void System_JobCallback(int range_start, int range_end, int thread_index, void* user) {
-    printf(" * %d - %d\n", range_start, range_end);
-}
-
 void System_Start() {
-    sx_alloc* allocator = sx_alloc_malloc();
-    sx_job_context_desc context_desc = {
-        .num_threads = 12,
-        .max_fibers = 1024,
-        .fiber_stack_sz = 1 * 1024 * 1024,
-        .thread_init_cb = NULL,
-        .thread_shutdown_cb = NULL,
-        .thread_user_data = NULL,
-    };
-
-    sx_job_context* context = sx_job_create_context(allocator, &context_desc);
-
-    sx_job_t job = sx_job_dispatch(context, 1024, System_JobCallback, NULL, SX_JOB_PRIORITY_NORMAL, 0);
-    sx_job_wait_and_del(context, job);
-
-    sx_job_destroy_context(context, allocator);
-
     Server_Start();
     Client_Start();
 }
